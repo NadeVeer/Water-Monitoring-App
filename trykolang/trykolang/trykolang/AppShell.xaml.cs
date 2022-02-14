cff1aff91d32a8bc6de108f04872439fcd4fd659
NadeVeer <98113699+NadeVeer@users.noreply.github.com>
2022-02-14 23:08:48 +0800

    trykolang

---
+++ trykolang/trykolang/trykolang/AppShell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using trykolang.ViewModels;
+using trykolang.Views;
+using Xamarin.Forms;
+
+namespace trykolang
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ trykolang/trykolang/trykolang/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using trykolang.ViewModels;
+using Xamarin.Forms;
+
+namespace trykolang.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
